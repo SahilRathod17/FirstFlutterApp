5015418d19020da70c07f67f9e78eac019f2cc7c
Sahil <samael17111@gmail.com>
2022-08-02 01:02:16 +0530

    Day3 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,55 +1,17 @@
 import 'package:flutter/material.dart';
 
 class login_page extends StatelessWidget {
+  const login_page({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.white,
       child: Column(
+        children: [
           Image.asset(
             "assets/images/login_image.png",
             fit: BoxFit.cover,
           ),
-          SizedBox(
-            height: 15,
-          ),
-                 TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-        Text(
-            "Welcome!!!",
-            style: TextStyle(
-              fontSize: 25,
-            ),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                      hintText: "Enter Your Username", labelText: "UserName"),
-                ),
-        children: [
-                     hintText: "Enter Your Password",
-                    labelText: "Password",
-                  ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    print("YESSSSS");
-                  },
-                  child: Text("LOGIN"),
-                )
-              ],
-            ),
-          )
         ],
       ),
     );
